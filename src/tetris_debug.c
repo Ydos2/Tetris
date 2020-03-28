c915f8dd2f68aa89f7703a8fe8f1d07ce677083c
Vitor Hugo Moreira Reis <vitor-hugo.moreira-reis@epitech.eu>
2020-03-28 14:45:43 +0100

    [MOD] help_page.c - Fixed an error with the help page

--- src/tetris_debug.c
+++ src/tetris_debug.c
@@ -68,10 +68,7 @@ void tetriminos_display(arguments_t *arguments, tetri_t *tetrimino)
 {
     for (int i = 0; i < arguments->nbr_tets; i++) {
         my_putstr("Tetriminos : Name ");
-        if (my_strlen(tetrimino[i].name) == 0)
-            my_putstr("(null)");
-        else
-            my_putstr(tetrimino[i].name);
+        my_putstr(tetrimino[i].name);
         my_putstr(" : ");
         if (tetrimino[i].error == 0)
             individual_display(tetrimino[i]);
